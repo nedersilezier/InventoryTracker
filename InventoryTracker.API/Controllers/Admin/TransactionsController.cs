820a44b2ff9e64d3fee9093196f3ef1703d95daa
Poecilotheria <249173782+nedersilezier@users.noreply.github.com>
2026-04-08 22:45:43 +0200

    feat(transactions): Add TransactionsController

---
+++ InventoryTracker.API/Controllers/Admin/TransactionsController.cs
@@ -0,0 +1,44 @@
+﻿using InventoryTracker.Application.Features.Transactions.Commands.CreateTransaction;
+using InventoryTracker.Application.Features.Transactions.Queries.GetTransactions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryTracker.API.Controllers.Admin
+{
+    [ApiController]
+    [Route("api/admin/transactions")]
+    [Authorize(Roles = "Admin")]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public TransactionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTransactions(CancellationToken cancellationToken)
+        {
+            var transactions = await _mediator.Send(new GetTransactionsQuery(), cancellationToken);
+            return Ok(transactions);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetTransactionById(Guid id, CancellationToken cancellationToken)
+        {
+            var transaction = await _mediator.Send(new GetTransactionByIdQuery(id), cancellationToken);
+            if (transaction == null)
+                return NotFound();
+            return Ok(transaction);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTransaction(CreateTransactionCommand command, CancellationToken cancellationToken)
+        {
+            var transaction = await _mediator.Send(command, cancellationToken);
+            return CreatedAtAction(nameof(GetTransactionById), new { id = transaction.TransactionId }, transaction);
+        }
+    }
+}
